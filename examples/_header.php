8eeab89354660b09e46857d958f90abc9bc2b3e3
Justin <justincyork@gmail.com>
2016-08-09 19:43:27 +0000

    sample app index page

--- examples/_header.php
+++ examples/_header.php
@@ -4,14 +4,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>FamilySearch PHP lite SDK Examples</title>
+    <title>FamilySearch PHP Lite SDK Examples</title>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
     <nav class="navbar navbar-default navbar-static-top">
       <div class="container">
         <div class="navbar-header">
-          <a class="navbar-brand" href="/">FamilySearch PHP lite SDK Examples</a>
+          <a class="navbar-brand" href="/">FamilySearch PHP Lite SDK Examples</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <p class="navbar-text navbar-right"><a class="navbar-link" href="https://github.com/york-solutions/fs-php-lite" target="_blank">SDK</a></p>
         </div>
       </div>
     </nav>
---
+++ examples/index.php
@@ -0,0 +1,12 @@
+<?php
+include_once '_includes.php';
+include_once '_header.php';
+?>
+
+<div class="jumbotron">
+  <p>Start by clicking "Authentication" in the side bar. Then explore the other examples.</p>
+  <p>At the bottom of each example page is a link to the example's source code.</p>
+</div>
+
+<?
+include_once '_footer.php';
